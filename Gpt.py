f858211c57b41df6b467fe7959abdcd630355611
chris <chris@aibots.au>
2026-05-28 11:19:07 +1000

    new file

--- Gpt.py
+++ Gpt.py
@@ -1,20 +1,16 @@
 import requests
+def AskGpt(question):
+    gptkey = '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'
+    url = 'https://api.openai.com/v1/responses'
+    payload = {
+        "model": "gpt-5.5",
+        "instructions": "Talk like a pirate who is an expert in comedy",
+        "input": question
+    }
+    headers = {
+        "Authorization": f"Bearer {gptkey}"
+    }
 
-gptkey = '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'
-url = 'https://api.openai.com/v1/responses'
-payload = {
-    "model": "gpt-5.5",
-    "input": "Tell me a really funny joke."
-}
-headers = {
-    "Authorization": f"Bearer {gptkey}"
-}
-
-response = requests.post(
-    url,
-    headers=headers,
-    json=payload
-)
-
-print(response.status_code)
-print(response.json())
+    response = requests.post(url, headers=headers, json=payload)
+    data = response.json()
+    return data['output'][1]['content'][0]['text']
